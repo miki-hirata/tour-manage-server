9bc06dc818b5b123b9d2ee5b69fc6fa06b5c5262
tangra <tangra.sd@gmail.com>
2022-01-18 16:58:36 +0900

    herokuエラー対応

--- models/index.js
+++ models/index.js
@@ -4,16 +4,28 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+//const env = process.env.NODE_ENV || 'development';
+//const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
+/* let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+} */
+const url =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:postgres@localhost:5432/tour_manage01";
+let sequelize = new Sequelize(url, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
 
 fs
   .readdirSync(__dirname)
---
+++ seeders/20220118075107-sample-user.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', [
+      {
+        name: 'ユーザー3',
+        password: 'kkokoreo',
+        removed: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+    
+    await queryInterface.bulkDelete('Users', null, {});
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
